c1216fb6af9cd41faea357a027fc2bbb59fab21d
ihfazhillah <mihfazhillah@gmail.com>
2017-01-05 23:10:12 +0700

    get_near, get_meaning param langsung query

--- scraper.py
+++ scraper.py
@@ -2,13 +2,13 @@ import requests
 from regex_patterns import near_results, meaning_results, entity
 
 
-def get_meaning(url):
-    resp = requests.get(url)
+def get_meaning(q):
+    resp = requests.get(Endpoint.AL_WASEETH.format(q=q))
     return meaning_results.search(resp.text).group(0)
 
 
-def get_near(url):
-    resp = requests.get(url)
+def get_near(q):
+    resp = requests.get(Endpoint.AL_WASEETH.format(q=q))
     return near_results.search(resp.text).group(0)
 
 
