7a8cf55a54259defdd8befa0c24fc3ecb0df9f84
ihfazhillah <mihfazhillah@gmail.com>
2017-01-09 09:27:47 +0700

    mengunduh pencarian dan save ke db

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
-from scraper import get_meaning, Endpoint
-
+from flask import Flask, render_template_string
+from scraper import get_meaning
+import timeit
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    url = Endpoint.AL_WASEETH.format(q='عين')
-    resp = get_meaning(url)
-    return resp
+    # url = Endpoint.AL_WASEETH.format(q='كرم')
+    start = timeit.timeit()
+    resp = get_meaning('فكاهة')
+    end = timeit.timeit()
+    elapsed = start - end
+    return render_template_string("""
+        <p>elapsed time: {{ elapsed }}</p>
+        <h2>نتائج عن بحثك....</h2>
+        {% autoescape false %}
+        {% for hsl in resp %}
+        <h3>{{hsl[0]}}</h3>
+        <p>{{hsl[1].replace('\n', '<br/>')}}</p>
+        {% endfor %}
+        {% endautoescape %}""", resp=resp, elapsed=elapsed)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -1,10 +1,73 @@
 import requests
 from regex_patterns import near_results, meaning_results, entity
+import sqlite3
+import os
+
+EXPIRED_AFTER = 1 # in days
+
+
+conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'req.db'))
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS
+               pencarian(
+               timestamp DATETIME NOT NULL,
+               query TEXT NOT NULL,
+               id INTEGER PRIMARY KEY AUTOINCREMENT)
+               """)
+cur.execute("""
+               CREATE TABLE IF NOT EXISTS
+               hasil(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               query_id INTEGER,
+               label TEXT NOT NULL,
+               arti TEXT NOT NULL,
+               FOREIGN KEY(query_id) REFERENCES pencarian(id))
+""")
+
+conn.commit()
+conn.close()
 
 
 def get_meaning(q):
-    resp = requests.get(Endpoint.AL_WASEETH.format(q=q))
-    return meaning_results.search(resp.text).group(0)
+    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'req.db'))
+    cur = conn.cursor()
+    query_db = cur.execute('''SELECT * FROM pencarian
+                             WHERE query=? AND
+                             CAST (
+                             (julianday('now', 'localtime') - julianday(timestamp))
+                             AS INTEGER) <= ?''', (q, EXPIRED_AFTER))
+
+    if query_db.fetchone() is None:
+        cur.execute('''INSERT INTO pencarian(timestamp, query)
+                       VALUES(date('now', 'localtime'), ?)''', (q,))
+        conn.commit()
+
+        resp = requests.get(Endpoint.AL_WASEETH.format(q=q))
+
+        entity = list(get_entity(meaning_results.search(resp.text).group(0)))
+        entities = [(x.group('label'),
+                     x.group('arti').replace('<br/>', '\n'),
+                     q) for x in entity]
+
+        cur.executemany('''INSERT INTO hasil(label, arti, query_id)
+                           VALUES(?,?,
+                           (SELECT id FROM pencarian WHERE query=?)
+                           )''', entities)
+        conn.commit()
+
+        cur.execute('''SELECT label, arti FROM hasil
+                       WHERE query_id=(
+                       SELECT id FROM pencarian WHERE query=?)
+                       ''', (q,))
+
+        return cur.fetchall()
+
+    cur.execute('''SELECT label, arti FROM hasil
+                   WHERE query_id=(
+                   SELECT id FROM pencarian WHERE query=?)
+                   ''', (q,))
+    return cur.fetchall()
 
 
 def get_near(q):
