dbaf08ae83df199d1a8c215278f3870cf372db6c
ihfazhillah <mihfazhillah@gmail.com>
2017-01-14 19:25:01 +0700

    menggunakan models baru

--- scraper.py
+++ scraper.py
@@ -2,47 +2,17 @@ import requests
 from regex_patterns import near_results, meaning_results, entity
 import sqlite3
 import os
-from models import Hasil
+from models import Hasil, Pencarian, create_tables
 
-EXPIRED_AFTER = 1 # in days
+EXPIRED_AFTER = 10 # in days
 
-
-conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'req.db'))
-cur = conn.cursor()
-
-cur.execute("""CREATE TABLE IF NOT EXISTS
-               pencarian(
-               timestamp DATETIME NOT NULL,
-               query TEXT NOT NULL,
-               id INTEGER PRIMARY KEY AUTOINCREMENT)
-               """)
-cur.execute("""
-               CREATE TABLE IF NOT EXISTS
-               hasil(
-               id INTEGER PRIMARY KEY AUTOINCREMENT,
-               query_id INTEGER,
-               label TEXT NOT NULL,
-               arti TEXT NOT NULL,
-               FOREIGN KEY(query_id) REFERENCES pencarian(id))
-""")
-
-conn.commit()
-conn.close()
-
+create_tables()
 
 def get_meaning(q):
-    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'req.db'))
-    cur = conn.cursor()
-    query_db = cur.execute('''SELECT * FROM pencarian
-                             WHERE query=? AND
-                             CAST (
-                             (julianday('now', 'localtime') - julianday(timestamp))
-                             AS INTEGER) <= ?''', (q, EXPIRED_AFTER))
+    query_db = Hasil.select(q, EXPIRED_AFTER)
 
     if query_db.fetchone() is None:
-        cur.execute('''INSERT INTO pencarian(timestamp, query)
-                       VALUES(date('now', 'localtime'), ?)''', (q,))
-        conn.commit()
+        Pencarian.insert(q)
 
         resp = requests.get(Endpoint.AL_WASEETH.format(q=q))
 
@@ -51,24 +21,14 @@ def get_meaning(q):
                      x.group('arti').replace('<br/>', '\n'),
                      q) for x in entity]
 
-        cur.executemany('''INSERT INTO hasil(label, arti, query_id)
-                           VALUES(?,?,
-                           (SELECT id FROM pencarian WHERE query=?)
-                           )''', entities)
-        conn.commit()
+        Hasil.insert_many(entities)
 
-        cur.execute('''SELECT label, arti FROM hasil
-                       WHERE query_id=(
-                       SELECT id FROM pencarian WHERE query=?)
-                       ''', (q,))
+        rows = Hasil.select(q, expire_after=EXPIRED_AFTER)
 
-        return cur.fetchall()
+        return rows.fetchall()
 
-    cur.execute('''SELECT label, arti FROM hasil
-                   WHERE query_id=(
-                   SELECT id FROM pencarian WHERE query=?)
-                   ''', (q,))
-    return cur.fetchall()
+    rows = Hasil.select(q, EXPIRED_AFTER)
+    return rows.fetchall()
 
 
 def get_near(q):
