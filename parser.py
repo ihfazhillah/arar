02f50ccf115ac9d52efe15dc6d8c01615181be1a
ihfazhillah <mihfazhillah@gmail.com>
2017-01-15 07:32:42 +0700

    menambah strip_tag dan mengubah menjadi staticmethod dan dokumentasi

--- parser.py
+++ parser.py
@@ -1,26 +1,47 @@
+"""Parser module, disini ada parser object"""
 from regex_patterns import entity, meaning_results, tags
 
 
 class Parser(object):
+    """Parser object, semua logic tentang parsing disini"""
 
     def __init__(self, src):
         self.src = src
 
-    def meaning(self, src):
+    @staticmethod
+    def meaning(src):
+        """mendapatkan result dengan class=meaning_results
+        src: source code dari html"""
         mean = meaning_results.search(src).group(0)
         return mean
 
-    def entity(self, mean):
+    @staticmethod
+    def entity(mean):
+        """Mendapatkan entity dari meaning...
+
+        mean: kembalian dari Parser.meaning"""
         entities = entity.finditer(mean)
         return entities
 
     def get_entities_with_query(self, query):
+        """Mengembalikan tuple didalam list.
+        tuple ada tiga item (secara urut):
+        1. label
+        2. arti
+        3. query
+
+        bila ditemukan, bila tidak akan mereturn None"""
         try:
-            entities = list(self.entity(self.meaning(self.src)))
+            entities = list(Parser.entity(Parser.meaning(self.src)))
         except AttributeError:
             return
 
         return [(x.group('label'),
                  x.group('arti').replace('<br/>', '\n'),
-                query,
+                 query,
                 ) for x in entities]
+
+    @staticmethod
+    def strip_tag(src):
+        """strip tags"""
+        return tags.sub("", src)
